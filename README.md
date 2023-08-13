58506dafa961ecf90c23cb474f44302e6bc3c712
pakkhaphonxvi <pakkhaphon.dev@gmail.com>
2023-08-14 00:41:02 +0700

    no message

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a simple program written in Go that encode string into a format suitable for sending SMS.
+It take a string as input and convert each character into hexadecimal, with percent encoding as necessary.
+
+## Usage
+
+1. Make sure you have Go installed on your system.
+2. Clone or download this repository.
+3. Open a terminal and navigate to the directory containing the code.
+
+### Running the Program
+
+Run the following command to execute the program:
+
+```shell
+go run main.go
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// inputString := "A"
+	// inputString := "ก"
+	// inputString := "あ"
+
+	fmt.Print("Enter the string to encode: ")
+	var inputString string
+	fmt.Scanln(&inputString)
+	
+	encodedHex := encodeToHex(inputString)
+	fmt.Println("Encoded:", encodedHex)
+}
+
+func encodeToHex(input string) string {
+	var encodedHex string
+	var putPercent string
+	var sumEncode string
+	var formatFound bool = false
+	var char_2_count int = 0
+
+	for _, char_1 := range input {
+		encodedHex = fmt.Sprintf("%X", char_1)
+		for _, char_2 := range encodedHex {
+			char_2_count++
+			if (len(encodedHex) > 2 && !(formatFound)) {
+				if (len(encodedHex) == 3) {
+					putPercent += "%0" + string(char_2) + "%"
+					formatFound = true
+				} else if (len(encodedHex) == 4 && (char_2_count<2)) {
+					putPercent += "%" + string(char_2)
+				} else if (len(encodedHex) == 4 && (char_2_count==2)) {
+					putPercent += string(char_2) + "%"
+					formatFound = true
+				}
+			} else if (formatFound) {
+				putPercent += string(char_2)
+			} else {
+				putPercent += "%00%" + string(char_2)
+				formatFound = true
+			}
+			fmt.Println(char_2_count)
+		}
+		sumEncode += putPercent
+		putPercent = ""
+		formatFound = false
+	}
+	return sumEncode
+}
